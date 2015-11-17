42ce858c57be1981deee31aa8eea99c159206746
Marvin Hübner <mjh@bergwerk.ag>
2015-11-17 18:25:40 +0100

    [task] add html template

--- html/partials/footer.php
+++ html/partials/footer.php
@@ -1,5 +1,3 @@
-<?php include('basic.php') ?>
-
 <script src="<?php echo $distributionJsFiles ?>app.js"></script>
 
 </body>
--- html/partials/header.php
+++ html/partials/header.php
@@ -1,11 +1,10 @@
-<?php include('basic.php') ?>
+<?php include_once('basic.php'); ?>
 
 <!DOCTYPE html>
 <html>
 <head lang="de">
-    <!---->
     <meta charset="UTF-8">
-    <title>Foundation Product</title>
+    <title>Src-Kit HTML Dummy</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $distributionCssFiles ?>app.min.css"/>
