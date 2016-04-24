71dbf248b4c01cefea239f125c5d44672b3f6f80
Marvin Huebner <mjh@bergwerk.ag>
2016-04-24 22:54:47 +0200

    add .editorconfig and refector code style

--- src/js/snipptes.js
+++ src/js/snipptes.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 });
 
 function setAnchorActive() {
 	var getPath = window.location.pathname;
-	var path = getPath.replace('/','');
+	var path = getPath.replace('/', '');
 
-	$('nav a').each(function(){
+	$('nav a').each(function () {
 		var anchor = $(this);
 
 		var getHref = anchor.attr('href');
