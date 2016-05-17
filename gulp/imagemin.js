0bd6877fb470db9317998e56fca96f05074041e2
Marvin Huebner <mjh@bergwerk.ag>
2016-05-17 14:54:59 +0200

    change editor config

--- gulp/imagemin.js
+++ gulp/imagemin.js
@@ -5,16 +5,16 @@ var pngquant = require('imagemin-pngquant');
 gulp.task('img', imageTask);
 
 function imageTask() {
-	var compileImages = function (destPath) {
-		gulp.src(sourcePath + 'assets/img/*')
-			.pipe(imagemin({
-				progressive: true,
-				use: [pngquant()]
-			}))
-			.pipe(gulp.dest(destPath + '/img'));
-	};
+    var compileImages = function (destPath) {
+        gulp.src(sourcePath + 'assets/img/*')
+            .pipe(imagemin({
+                progressive: true,
+                use: [pngquant()]
+            }))
+            .pipe(gulp.dest(destPath + '/img'));
+    };
 
-	compileImages('public/');
+    compileImages('public/');
 }
 
 
--- gulp/scripts.js
+++ gulp/scripts.js
@@ -8,32 +8,32 @@ var babel = require('gulp-babel');
 gulp.task('scripts', scriptsTask);
 
 function scriptsTask() {
-	var compileScripts = function (files, targetFile) {
+    var compileScripts = function (files, targetFile) {
 
-		switch (output) {
-			case 'normal':
-				gulp.src(files)
-					.pipe(plumber())
-					.pipe(sourcemaps.init())
-					.pipe(babel())
-					.pipe(concat(targetFile + '.js'))
-					.pipe(sourcemaps.write('./'))
-					.pipe(gulp.dest(destinationPath + 'js'));
-				break;
-			case 'minified':
-				gulp.src(files)
-					.pipe(plumber())
-					.pipe(sourcemaps.init())
-					.pipe(babel())
-					.pipe(concat(targetFile + '.js'))
-					.pipe(uglify())
-					//.pipe(rename({suffix: '.min'}))
-					.pipe(sourcemaps.write('./'))
-					.pipe(gulp.dest(destinationPath + 'js'));
-				break;
-		}
+        switch (output) {
+            case 'normal':
+                gulp.src(files)
+                    .pipe(plumber())
+                    .pipe(sourcemaps.init())
+                    .pipe(babel())
+                    .pipe(concat(targetFile + '.js'))
+                    .pipe(sourcemaps.write('./'))
+                    .pipe(gulp.dest(destinationPath + 'js'));
+                break;
+            case 'minified':
+                gulp.src(files)
+                    .pipe(plumber())
+                    .pipe(sourcemaps.init())
+                    .pipe(babel())
+                    .pipe(concat(targetFile + '.js'))
+                    .pipe(uglify())
+                    //.pipe(rename({suffix: '.min'}))
+                    .pipe(sourcemaps.write('./'))
+                    .pipe(gulp.dest(destinationPath + 'js'));
+                break;
+        }
 
-	};
+    };
 
-	compileScripts(jsFilesApp, 'app');
+    compileScripts(jsFilesApp, 'app');
 }
