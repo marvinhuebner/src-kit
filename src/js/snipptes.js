8e18ef940230ce609e55d60925cc4da5a4ee06af
Marvin Huebner <mjh@bergwerk.ag>
2016-06-03 15:28:47 +0200

    lower camel case

---
+++ src/js/snipptes.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+});
+
+function setAnchorActive() {
+    var getPath = window.location.pathname;
+    var path = getPath.replace('/', '');
+
+    $('nav a').each(function () {
+        var anchor = $(this);
+
+        var getHref = anchor.attr('href');
+
+        if (path == getHref) {
+            anchor.addClass('active');
+        }
+    })
+}
